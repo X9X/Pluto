21577543223988f728394748fb52f4ceca1dc86b
Young <199681@sh.lianjia.com>
2016-06-28 20:04:21 +0800

    add reset css

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
+    <!-- <img class="logo" src="./assets/logo.png"> -->
     <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <navi></navi>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import Navi from './components/Navi'
 
 export default {
   components: {
-    Hello
+    Hello,
+    Navi
   }
 }
 </script>
@@ -48,12 +34,11 @@ body {
   color: #2c3e50;
   margin-top: -100px;
   max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
+  font-family: "PingFang SC","苹方","Hiragino Sans GB","Microsoft Yahei","微软雅黑", "Microsoft Yahei UI", 'Segoe UI', Tahoma,SimSun, sans-serif;;
   text-align: center;
 }
 
 #app a {
-  color: #42b983;
   text-decoration: none;
 }
 
---
+++ src/components/Navi.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li v-for="item in navList"><a href="/" @click="linkTo()">{{item.linkName}}</a></li>
+        </ul>
+    </div>
+</template>
+<style type="text/css" scoped>
+    .nav {
+        display: inline-block;
+        width: 200px;
+        background-color: #333333;
+        font-size: 16px;
+        position: fixed;
+        z-index: 1000;
+        left: 0;
+        top:0;
+        bottom: 0;
+    }
+    .nav ul{
+        width: 100%;
+    }
+    .nav ul li {
+        list-style: none;
+        display:block;
+        width: 100%;
+        text-align: center;
+        padding: 20px 0;
+    }
+    .nav ul li a{
+        color:#ffffff;
+    }
+    .nav ul li:hover{
+        background-color: #42b983;
+    }
+</style>
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      navList: [{linkName: '首页'}, {linkName: '列表'}, {linkName: '发现'}]
+    }
+  },
+  methods () {
+    return {
+      linkTo () {
+        window.alert('where to link ?')
+      }
+    }
+  }
+}
+
+</script>
